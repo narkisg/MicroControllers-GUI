0272c094488c30ce58bb165114b9af28429038de
Adi <adi.straussa@gmail.co>
2020-10-11 19:17:17 +0300

    11

--- src/components/MsgCard.jsx
+++ src/components/MsgCard.jsx
@@ -1,32 +1,57 @@
-import { Card, CardContent } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardContent,
+  CircularProgress,
+  Fade,
+} from "@material-ui/core";
 import React from "react";
 
 export default function MsgCard(props) {
+  const [loading, setLoading] = React.useState(false);
   const { bootloader, process } = props;
   const sampleJSON = {
     object: process,
   };
+  const handleClickLoading = () => {
+    setLoading((prevLoading) => !prevLoading);
+  };
 
   return (
-
     <div>
-      {bootloader && (<Card>
+      <Card>
         <CardContent>
-          <h3>process arguments: </h3>
-          {/* text */}
-          {Object.keys(sampleJSON.object).map((key, i) => (
-              <p key={i}>
-                <span>{key}</span>
-                <span>: {sampleJSON.object[key]}</span>
-              </p>
-          ))}
+          <Fade
+            in={loading}
+            style={{
+              transitionDelay: loading ? "800ms" : "0ms",
+            }}
+            unmountOnExit>
+            <CircularProgress />
+          </Fade>
 
-          <h3>bootloader reply:</h3>
-          {/* text */}
-          <p>{bootloader}</p>
+          <Button onClick={handleClickLoading}>
+            {loading ? "Stop loading" : "Loading"}
+          </Button>
+
+          {bootloader && (
+            <div>
+              <h3>process arguments: </h3>
+              {/* text */}
+              {Object.keys(sampleJSON.object).map((key, i) => (
+                <p key={i}>
+                  <span>{key}</span>
+                  <span>: {sampleJSON.object[key]}</span>
+                </p>
+              ))}
+
+              <h3>bootloader reply:</h3>
+              {/* text */}
+              <p>{bootloader}</p>
+            </div>
+          )}
         </CardContent>
-      </Card>)}
-
+      </Card>
     </div>
   );
 }
