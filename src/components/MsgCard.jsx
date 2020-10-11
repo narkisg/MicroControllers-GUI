3bc5daf388b09505f177357cb6683f29d1ebfa1e
Adi <adi.straussa@gmail.co>
2020-10-11 19:30:58 +0300

    111

--- src/components/MsgCard.jsx
+++ src/components/MsgCard.jsx
@@ -16,42 +16,49 @@ export default function MsgCard(props) {
   const handleClickLoading = () => {
     setLoading((prevLoading) => !prevLoading);
   };
+  const booli = false;
 
   return (
     <div>
-      <Card>
-        <CardContent>
-          <Fade
-            in={loading}
-            style={{
-              transitionDelay: loading ? "800ms" : "0ms",
-            }}
-            unmountOnExit>
-            <CircularProgress />
-          </Fade>
+      <Button onClick={handleClickLoading}>
+        {loading ? "Stop loading" : "Loading"}
+      </Button>
+      {!loading && (
+        <div>
+          <Card>
+            <CardContent>
+              <div>
+                <h3>process arguments: </h3>
+                {/* text */}
+                {Object.keys(sampleJSON.object).map((key, i) => (
+                  <p key={i}>
+                    <span>{key}</span>
+                    <span>: {sampleJSON.object[key]}</span>
+                  </p>
+                ))}
 
-          <Button onClick={handleClickLoading}>
-            {loading ? "Stop loading" : "Loading"}
-          </Button>
-
-          {bootloader && (
-            <div>
-              <h3>process arguments: </h3>
-              {/* text */}
-              {Object.keys(sampleJSON.object).map((key, i) => (
-                <p key={i}>
-                  <span>{key}</span>
-                  <span>: {sampleJSON.object[key]}</span>
-                </p>
-              ))}
-
-              <h3>bootloader reply:</h3>
-              {/* text */}
-              <p>{bootloader}</p>
-            </div>
-          )}
-        </CardContent>
-      </Card>
+                <h3>bootloader reply:</h3>
+                {/* text */}
+                <p>{bootloader}</p>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      )}
+      {loading && (
+        <div>
+          <center>
+            <Fade
+              in={loading}
+              style={{
+                transitionDelay: loading ? "800ms" : "0ms",
+              }}
+              unmountOnExit>
+              <CircularProgress />
+            </Fade>
+          </center>
+        </div>
+      )}
     </div>
   );
 }
